25e11250cc83723664fb29179ba8643e21219218
hartless <behart@gmail.com>
2019-02-09 15:59:33 -0500

    scss updates

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,38 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
+      <router-link to="/">Home</router-link>|
       <router-link to="/about">About</router-link>
     </div>
-    <router-view />
+    <router-view/>
   </div>
 </template>
 
 <style lang="scss">
+html,
+body {
+  margin: 0;
+  padding: 0;
+}
 #app {
+  padding: 3em;
   font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #ddeedf;
+  background-color: #000;
+}
+a {
+  text-decoration: none;
 }
 #nav {
   padding: 30px;
   a {
     font-weight: bold;
-    color: #2c3e50;
+    color: #5b8f70;
     &.router-link-exact-active {
-      color: #42b983;
+      color: #7cddb1;
     }
   }
 }
