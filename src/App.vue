aca4a0c288e58b3db1e6c69dae7cbd7f0ded2c49
hartless <behart@gmail.com>
2019-02-10 14:32:32 -0500

    WIP routing, signin process

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,12 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link>|
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <router-view />
   </div>
 </template>
 
 <style lang="scss">
+@import "@/global-styles/colors.scss";
+@import "@/global-styles/typography.scss";
 html,
 body {
   margin: 0;
@@ -20,8 +18,6 @@ body {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #ddeedf;
-  background-color: #000;
 }
 a {
   text-decoration: none;
---
+++ src/views/SignInFlow/SignIn.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="container">
+    <h1>Signin</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "SignIn"
+};
+</script>
+
+<style scoped lang="scss">
+.container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  min-height: 100vh;
+}
+</style>
